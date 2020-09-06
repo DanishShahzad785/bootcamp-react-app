30af6081e86f72bb9b783da0212434c56037196e
DanishShahzad785 <67574423+DanishShahzad785@users.noreply.github.com>
2020-09-06 18:07:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Danish Shahzad
-      </h1>
+      <h1>Danish Shahzad </h1>
     </div>
   );
 }
